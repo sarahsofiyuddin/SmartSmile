7955a6ac7da0b3a350ca3e8bbd330cfe38dc0144
sarahsofiyuddin <sarahsyazana.work@gmail.com>
2025-04-11 00:57:33 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/smartsmile/Constants.java
@@ -0,0 +1,8 @@
+package com.example.smartsmile;
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.127.1:8080/SmartSmile/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+}
